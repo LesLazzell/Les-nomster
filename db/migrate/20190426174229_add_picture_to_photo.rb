2e6cb70f9627231c074cdb9156e16d05fb83d8dc
Les Lazzell <llazzell4664@outlook.com>
2019-04-26 21:44:37 +0000

    39 trial and error def

--- db/migrate/20190426174229_add_picture_to_photo.rb
+++ db/migrate/20190426174229_add_picture_to_photo.rb
@@ -1,6 +1,9 @@
 class AddPictureToPhoto < ActiveRecord::Migration[5.2]
   def change
+    create_table :photos do |f|
+       f.input :picture 
+    end
+
     add_column :photos, :picture, :string
-    <%= f.input :picture %>
   end
 end
