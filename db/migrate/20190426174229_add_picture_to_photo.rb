101cd4cd69309b18032a5b7be36b4fc89c7c2425
Les Lazzell <llazzell4664@outlook.com>
2019-04-26 22:25:44 +0000

    Friday Lesson 39 with Ben

--- db/migrate/20190426174229_add_picture_to_photo.rb
+++ db/migrate/20190426174229_add_picture_to_photo.rb
@@ -1,9 +1,5 @@
 class AddPictureToPhoto < ActiveRecord::Migration[5.2]
   def change
-    create_table :photos do |f|
-       f.input :picture 
-    end
-
     add_column :photos, :picture, :string
   end
 end
